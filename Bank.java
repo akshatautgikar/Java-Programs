45a336b91d425e53b2ab12b8158518e6c8f72e37
[akshatautgikar] <[akshatautgikar11@gmail.com]>
2024-08-27 16:56:34 +0530

    adding files to Java Programs

---
+++ Bank.java
@@ -0,0 +1,17 @@
+/* This Program is sample of a simple Banking Application*/
+
+class Account{
+
+String name;
+int accno;
+}
+
+public class Bank{
+
+public static void main(String[] args){
+
+Account ac = new Account();
+
+
+}
+}
